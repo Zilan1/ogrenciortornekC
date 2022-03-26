4ba1c3bfc7e888c3670b5ff9f6dc8b33f578889f
Zilan1 <79017444+Zilan1@users.noreply.github.com>
2022-03-26 15:38:12 +0300

    sum

---
+++ proje60.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	
+	
+	//printf("*****Ogrenci Ortalama Hesaplama*****\n\n");
+	
+	//int s1,s2,s3,p,ort;
+	
+	//printf("Birinci Sinav Notunuzu Giriniz: ");
+	//scanf("%d",&s1);
+	
+	//printf("IKinci Sinav Notunuzu Giriniz: ");
+	//scanf("%d",&s2);
+	
+	//printf("Proje Notunuzu Giriniz: ");
+	//scanf("%d",&p);
+	
+	//ort=(s1+s2+p)/3;
+	
+	//printf("Ortalamaniz: %d",ort);
+	
+	//if(ort>=50)
+	//{
+		//printf(" Tebrikler Gectiniz!");
+	//}
+	//else
+	//{
+		//printf(" Maalesef Kaldiniz.");
+	//}
+	
+	
+	
+	
+	
+	
+	
+	//printf("*****Ogrenci Ortalama Hesaplama*****\n\n");
+	
+	//int s1,s2,s3,p,ort;
+	
+	//printf("Birinci Sinav Notunuzu Giriniz: ");
+	//scanf("%d",&s1);
+	
+	//printf("IKinci Sinav Notunuzu Giriniz: ");
+	//scanf("%d",&s2);
+	
+	//printf("Proje Notunuzu Giriniz: ");
+	//scanf("%d",&p);
+	
+	//ort=(s1+s2+p)/3;
+	
+	//printf("Ortalamaniz: %d",ort);
+	
+	
+	
+	
+	
+	//int s1,s2,t;
+	
+	//printf("Birinci Sayiyi Giriniz: ");
+	//scanf("%d",&s1);
+	
+	//printf("Ikinci Sayiyi Giriniz: ");
+	//scanf("%d",&s2);
+	
+	//t=s1+s2;
+	
+    //if(t>=50)
+	//{
+		//printf("Sayilarin Toplami: %d - 50'den buyuk.",t);
+	//}
+	
+	//else
+	//{
+		//printf("Sayilarin Toplami: %d - 50'den kucuk.",t);
+	//}
+	
+	return 0;
+}
